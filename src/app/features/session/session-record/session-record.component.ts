d3c6a38dc354b80a911a46708d3eef3ef5e66327
FatherOfCurses <colin.principe@gmail.com>
2021-05-06 22:15:22 -0400

    Adding further component files

---
+++ src/app/features/session/session-record/session-record.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-session-record',
+  templateUrl: './session-record.component.html',
+  styleUrls: ['./session-record.component.scss']
+})
+export class SessionRecordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
